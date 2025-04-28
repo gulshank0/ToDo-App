ccb4ef01bff4ea14c9015aadd002c65bb12d2530
gulshank0 <gulshanbahadur002@gmail.com>
2025-04-29 02:03:56 +0530

    github action

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import todoRoutes from "./routes/todoRoutes.js";
 import authMiddleware from "./middlewares/authMiddleware.js";
 
 const app = express();
-const PORT = process.env.PORT || 5003;
+const PORT = process.env.PORT || 5000;
 
 // Get the file path from the URL of the current module
 const __filename = fileURLToPath(import.meta.url);
